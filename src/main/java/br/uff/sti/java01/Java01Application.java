d2208a2cb090eb07ff6ee26603572ebf0f2742d3
awhittick <awhittick@gmail.com>
2018-07-26 15:37:27 -0300

    new commit1

---
+++ src/main/java/br/uff/sti/java01/Java01Application.java
@@ -0,0 +1,12 @@
+package br.uff.sti.java01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java01Application.class, args);
+	}
+}
